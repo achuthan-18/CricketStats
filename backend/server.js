35abc304a3d855be8866bf4b2148937eac6dfd78
ACHUTHAN T B <167844870+achuthan-18@users.noreply.github.com>
2025-06-08 21:09:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,8 @@ const bowlingRoute = require('./routes/bowlingRoutes');
 const cors = require('cors');
 
 const app = express();
-const allowedOrigins = ['http://localhost:5173', 'https://achuthancricketstats.onrender.com'];
-
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); 
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+    origin: "https://achuthancricketstats.onrender.com"
 }));
 
 app.use(express.json());
